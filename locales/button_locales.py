a3b2d858e2778cf29df5458f3493a0279a91e0a3
matttttthewy <matthewgritsik@gmail.com>
2025-06-06 17:27:06 +0300

    Create button_locales.py

---
+++ locales/button_locales.py
@@ -0,0 +1,78 @@
+
+class MainKeyboardLocales:
+    texts = {
+            "ru": {
+                "hotel": "🏨 О нашем отеле",
+                "calculator": "💰 Калькулятор стоимости",
+                "location": "📍 Как добраться",
+                "contact": "📞 Связаться с нами",
+                "faq": "❓ Часто задаваемые вопросы",
+                "admin": "🔒 Админ панель"
+            },
+            "en": {
+                "hotel": "🏨 About our hotel",
+                "calculator": "💰 Price calculator",
+                "location": "📍 How to get there",
+                "contact": "📞 Contact us",
+                "faq": "❓ Frequently asked questions",
+                "admin": "🔒 Admin panel"
+            }
+        }
+
+
+class AdminKeyboardLocales:
+    texts = {
+            "ru": {
+                "stats": "📊 Статистика",
+                "broadcast": "📢 Рассылка",
+                "rfaq": "❓ Редактор FAQ",
+                "back": "⬅️ Назад"
+            },
+            "en": {
+                "stats": "📊 Stats",
+                "broadcast": "📢 Broadcast",
+                "rfaq": "❓ FAQ editor",
+                "back": "⬅️ Back"
+            }
+        }
+
+class FAQModerateKeyboardLocales:
+    menu_texts = {
+            "ru": {
+                "add": "➕ Добавить вопрос",
+                "remove": "➖ Удалить вопрос",
+                "edit": "✏️ Редактировать вопрос",
+                "back": "⬅️ Назад"
+            },
+            "en": {
+                "add": "➕ Add question",
+                "remove": "➖ Remove question",
+                "edit": "✏️ Edit question",
+                "back": "⬅️ Back"
+            }
+        }
+
+    approve_delete_faq_texts = {
+            "ru": {
+                "yes": "✅ Да",
+                "no": "❌ Нет"
+            },
+            "en": {
+                "yes": "✅ Yes",
+                "no": "❌ No"
+            }
+        }
+    
+    edit_faq_texts = {
+            "ru": {
+                "edit_question": "Редактировать вопрос",
+                "edit_answer": "Редактировать ответ",
+                "back": "⬅️ Назад"
+            },
+            "en": {
+                "edit_question": "Edit question",
+                "edit_answer": "Edit answer",
+                "back": "⬅️ Back"
+            }
+        }
+
