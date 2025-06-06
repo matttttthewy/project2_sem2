13f64296099ac1f06e39b146a90b1fa77ade8abd
matttttthewy <matthewgritsik@gmail.com>
2025-06-06 17:20:53 +0300

    Add files via upload

---
+++ config/bot_config.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = eval(os.getenv("ADMIN_IDS"))
+FAQ_STORAGE_PATH = os.getenv("FAQ_STORAGE_PATH")
+DB_PATH = os.getenv("DB_PATH")
+GOOGLE_CREDENTIALS_PATH = os.getenv("GOOGLE_CREDENTIALS_PATH")
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+
+
+
+
+
+'''BOT_TOKEN = "7269394176:AAHbtUCPD2VQg5wlB4kN-hFrIyoLpojyplU"
+ADMIN_IDS = {429618569, 739758783}
+FAQ_STORAGE_PATH = "storage/faq.json"'''
