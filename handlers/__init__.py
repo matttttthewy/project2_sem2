6d1c91ffae515b7d593ca01845dc77620c4c017c
matttttthewy <matthewgritsik@gmail.com>
2025-06-07 19:30:43 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,5 @@
+from .user_handlers import user_router
+from .admin_handlers import admin_router
+from .general_hanlders import general_router
 
+__all__ = ["user_router", "admin_router", "general_router"]
