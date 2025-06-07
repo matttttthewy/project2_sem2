7229efe2c44c11dba70cab25730bd6f6afcba43e
matttttthewy <matthewgritsik@gmail.com>
2025-06-07 19:31:31 +0300

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,15 @@
+from .keyboards import *
+from .user_faq_keyboard import *
+from .language_keyboard import *
+from .contact_admin_keyboard import *
 
+
+__all__ = [
+    "MainKeyboard",
+    "AdminKeyboard",
+    "FAQModerateKeyboard",
+    "UserFaqKeyboard",
+    "LanguageKeyboard",
+    "RoomsKeyboard",
+    "LANGUAGE_MAPPING"
+]
